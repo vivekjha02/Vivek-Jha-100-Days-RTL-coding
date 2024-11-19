c7cf5f420ddbc9571e03b3911cf39cb7c216b297
Vivek Kumar Jha <110534044+vivekjha02@users.noreply.github.com>
2024-11-19 23:43:39 +0530

    Add files via upload

---
+++ Day16 4 bit divider/tb.v
@@ -0,0 +1,16 @@
+module tb;
+    reg [3:0]dividend,divisor;
+    wire [3:0]quotient,remainder;
+
+    divide dut(dividend,divisor,quotient,remainder);
+		always begin
+			
+			dividend =$random;
+			divisor =$random;
+            #10;
+		end
+    initial
+    begin $monitor("%0d / %0d = %0d with remainder %0d ",dividend,divisor,quotient,remainder);
+    #100 $finish;
+    end
+endmodule
